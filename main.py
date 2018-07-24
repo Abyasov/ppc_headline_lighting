c836be43b65327b2284f89255df248df8cf13c09
Abyasov Renat <abyasov.renat@gmail.com>
2018-07-25 01:46:55 +0500

    Экспериментируем в ноутбуке, пробуем pymorthy и яндексовский стемер

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
-v = pd.__version__
+# переменные на время разработки
+free_format_xls_file_path = '/test_data/free_format_file.xlsx'
+free_format_csv_file_path = 'test_data/free_format_file.csv'
 
-print('Hello, world! We are working with pandas version {}'.format(v))
+# data_xls = pd.read_excel(free_format_xls_file_path)
+# data_xls
+
+data_csv = pd.read_csv(free_format_csv_file_path)
+print(data_csv)
+
